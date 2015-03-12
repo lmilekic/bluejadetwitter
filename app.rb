9e4cc73cbfabfe1da62b3069d7a4e5efa9e297d5
recrudescence <calvinmwang@gmail.com>
2015-03-12 16:53:57 -0400

    super duper basic sign-up functionality (registration, save into db)

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments'
+require_relative 'models/user'
 
 get '/' do
   erb :welcome
+end
+
+post '/signup' do
+	@user = User.create(:username => params[:username],
+						 #:email => params[:email],
+						 :password => params[:password] )
+	if @user.save
+		redirect '/profile'
+	else
+		"Sorry, there was an error!"
+	end
 end
 
 get '/profile' do
--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tweets
+
+  def to_json
+  	super(:except => :password)
+  end
 end
