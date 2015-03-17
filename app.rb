a1786b887e1a3c07b70a4258abf98c57354c9cd0
Luka Milekic <lmilekic@gmail.com>
2015-03-17 12:51:18 -0400

    #1 after logging in user id is saved in session as session[:id] and I made a helper function current_user that returns current user or nil if there is none

--- app.rb
+++ app.rb
@@ -4,9 +4,15 @@ require './config/environments'
 require_relative 'models/user'
 require_relative 'models/tweet'
 
+enable :sessions
+
 get '/' do
   # if user is signed in then erg :homepage else erb :welcome
-  erb :welcome
+  if(current_user)
+    erb :homepage
+  else
+    erb :welcome
+  end
 end
 
 post '/api/v1/signup' do
@@ -14,19 +20,25 @@ post '/api/v1/signup' do
 						 #:email => params[:email],
 						 :password => params[:password] )
 	if user.save
+    session[:id] = user.id
 		redirect '/profile'
 	else
 		"Sorry, there was an error!"
 	end
 end
 
+post '/api/v1/login' do
+  user = User.where(:username => params[:username], :password => params[:password])
+  session[:id] = user.id
+end
+
 post '/api/v1/tweet' do
 	tweet = Tweet.create(:text => params[:tweet_text],
-							:user_id => 1, #temporarily for now, use userid 1
+							:user_id => session[:id], #temporarily for now, use userid 1
 							# later on it shoudl be something like:
 							# :reference => session[:userid]
 							# or something
-							:created_at => Time.now)
+							:created_at => Time.now) #I think created_at is auto_generated
 	if tweet.save
 		redirect back #refreshes
 	else
@@ -35,7 +47,11 @@ post '/api/v1/tweet' do
 end
 
 get '/profile' do
-  erb :profile
+  if(current_user)
+    erb :profile
+  else
+    redirect to('/')
+  end
 end
 
 get '/homepage' do
@@ -43,4 +59,13 @@ get '/homepage' do
 	@userTweets = Tweet.where(user_id: 1).to_a
 
 	erb :homepage
+end
+
+private
+def current_user
+  if(session[:id].nil?)
+    false
+  else
+    User.where(:id => session[:id])
+  end
 end
