75ff5fcd7fce78c87f7e8e86b0294942cc227341
Luka Milekic <lmilekic@gmail.com>
2015-03-19 16:42:26 -0400

    new head

--- app.rb
+++ app.rb
@@ -3,36 +3,53 @@ require 'sinatra/activerecord'
 require './config/environments'
 require_relative 'models/user'
 require_relative 'models/tweet'
+require_relative 'models/user_following_user'
 
 enable :sessions
+before do
+  puts "id is " + session[:id].to_s
+end
 
 get '/' do
-	session["user_id"] ||= nil
   # if user is signed in then erg :homepage else erb :welcome
-  @publicFeed = Tweet.all.to_a
-  erb :welcome
+  if(current_user)
+    redirect to('/homepage')
+    #@userTweets = []
+    #erb :homepage #This needs @userTweets to be defined
+  else
+    erb :welcome
+  end
 end
 
 post '/api/v1/signup' do
 	user = User.create(:username => params[:username],
-						 #:email => params[:email],
+						 :email => params[:email],
 						 :password => params[:password] )
-	session["user_id"] = user.id
-	session["username"] = user.username
 	if user.save
-		redirect "/profile/#{user.username}"
+    session[:id] = user.id
+		redirect '/profile'
 	else
 		"Sorry, there was an error!"
 	end
 end
 
+post '/api/v1/login' do
+  user = User.where(:email => params[:email], :password => params[:password]).first
+  if(user)
+    session[:id] = user.id
+    redirect to('/profile')
+  else
+    "there was an error"
+  end
+end
+
 post '/api/v1/tweet' do
 	tweet = Tweet.create(:text => params[:tweet_text],
-							:user_id => 1, #temporarily for now, use userid 1
+							:user_id => session[:id], #temporarily for now, use userid 1
 							# later on it shoudl be something like:
 							# :reference => session[:userid]
 							# or something
-							:created_at => Time.now)
+							:created_at => Time.now) #I think created_at is auto_generated
 	if tweet.save
 		redirect back #refreshes
 	else
@@ -40,18 +57,65 @@ post '/api/v1/tweet' do
 	end
 end
 
-get '/profile/:user' do
-	user = User.where(username: params[:user])
-	user_id = user[0].id
-	@username = params[:user]
-	@profileFeed = Tweet.where(user_id: user_id)
+#BELOW is close to what we want to have this post request doing later, but it currently relies on too many other things
 
-  erb :profile
+# post '/api/v1/follow/:id' do
+# 	#TO FIX LATER -- THIS CURRENTLY RECREATES THIS SAME FOLLOWING CONNECTION OVER AND OVER
+# 	#ALSO- need to add in reference to user id of the profile and user id of the person viewing the profile
+#  	stalk = UserFollowingUser.create(:user_id => session[:id],
+#  										:followed_user_id => params[:id])
+#  	if stalk.save
+#  		redirect back
+#  	else
+#  		"IT DIDN'T WORK"
+#  	end
+# end
+
+post '/api/v1/follow' do
+	#TO FIX LATER -- THIS CURRENTLY RECREATES THIS SAME FOLLOWING CONNECTION OVER AND OVER
+	#ALSO- need to add in reference to user id of the profile and user id of the person viewing the profile
+ 	stalk = UserFollowingUser.create(:user_id => 1,
+ 										:followed_user_id => 2)
+ 	if stalk.save
+ 		redirect back
+
+ 	else
+ 		"IT DIDN'T WORK"
+ 	end
+end
+
+post '/api/v1/unfollow' do
+	#TO FIX LATER -- THIS CURRENTLY DELETES THIS SAME FOLLOWING CONNECTION OVER AND OVER
+	#ALSO- need to add in reference to user id of the profile and user id of the person viewing the profile
+
+ 	User.find(1).user_following_users.destroy_all
+
+ 	redirect back
+end
+
+get '/profile/:user' do
+  user = User.where(username: params[:user])
+  user_id = user[0].id
+  @username = params[:user]
+  @profileFeed = Tweet.where(user_id: user_id)
 end
 
 get '/homepage' do
 
-	@userTweets = Tweet.where(user_id: 1).to_a
+	@userTweets = Tweet.where(user_id: session[:id]).to_a
 
 	erb :homepage
+end
+
+get '/logout' do
+  session[:id] = nil
+  redirect to('/')
+end
+private
+def current_user
+  if(session[:id].nil?)
+    false
+  else
+    User.where(:id => session[:id])
+  end
 end
