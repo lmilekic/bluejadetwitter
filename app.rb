ef1f3c8b0d4603a590cc6e6cc1b0988016513af5
recrudescence <calvinmwang@gmail.com>
2015-03-19 17:22:23 -0400

    #3: added tweet button on profile page, set to display if profile page is logged-in user's page

--- app.rb
+++ app.rb
@@ -26,7 +26,8 @@ post '/api/v1/signup' do
 						 :email => params[:email],
 						 :password => params[:password] )
 	if user.save
-    session[:id] = user.id
+    	session[:id] = user.id
+    	session[:username] = user.username
 		redirect '/profile'
 	else
 		"Sorry, there was an error!"
@@ -45,10 +46,7 @@ end
 
 post '/api/v1/tweet' do
 	tweet = Tweet.create(:text => params[:tweet_text],
-							:user_id => session["user_id"], #temporarily for now, use userid 1
-							# later on it shoudl be something like:
-							# :reference => session[:userid]
-							# or something
+							:user_id => session[:user_id], 
 							:created_at => Time.now) #I think created_at is auto_generated
 	if tweet.save
 		redirect back #refreshes
@@ -98,6 +96,7 @@ get '/profile/:user' do
   user_id = user[0].id
   @username = params[:user]
   @profileFeed = Tweet.where(user_id: user_id)
+  if @username === session[:username] then @self = true else @self = false end
 end
 
 get '/homepage' do
