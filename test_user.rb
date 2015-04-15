373a5fa0446d1528f0a23a4323e2090f246999f1
recrudescence <calvinmwang@gmail.com>
2015-04-15 19:12:46 -0400

    #78: updated id

--- test_user.rb
+++ test_user.rb
@@ -2,7 +2,7 @@ require 'faker'
 get '/test_tweet' do
 	if (Tweet.create(
 	  	:text => Faker::Hacker.say_something_smart,
-	  	:user_id => 1009,
+	  	:user_id => 1011,
 	  	:created_at => Time.now))
 		"created tweet"
 	else
@@ -12,14 +12,14 @@ end
 
 get '/test_follow' do
 	id = Random.rand(User.count)
-	if (UserFollowingUser.exists?(user_id: 1009, followed_user_id: id))
-		if (UserFollowingUser.where(:user_id => 1009, :followed_user_id => id).destroy_all)
+	if (UserFollowingUser.exists?(user_id: 1011, followed_user_id: id))
+		if (UserFollowingUser.where(:user_id => 1011, :followed_user_id => id).destroy_all)
 			"unfollowed #{id}"
 		else
 			"unfollow error with #{id}"
 		end
 	else
-		if (UserFollowingUser.create(:user_id => 1009, :followed_user_id => id))
+		if (UserFollowingUser.create(:user_id => 1011, :followed_user_id => id))
 			"follow relation created with #{id}"
 		else
 			"follow error with #{id}"
@@ -28,7 +28,7 @@ get '/test_follow' do
 end
 
 get '/reset' do
-	if (Tweet.where(user_id: 1009).destroy_all) && (UserFollowingUser.where(user_id: 1009).destroy_all)
+	if (Tweet.where(user_id: 1011).destroy_all) && (UserFollowingUser.where(user_id: 1011).destroy_all)
 		"reset test user's stuff"
 	else
 		"error resetting"
