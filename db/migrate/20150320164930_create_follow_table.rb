cf8802ce20b851208d853c185c1699d898bff268
Calvin Wang <cfourcalvin@gmail.com>
2015-04-30 16:18:20 -0400

    Update 20150320164930_create_follow_table.rb

--- db/migrate/20150320164930_create_follow_table.rb
+++ db/migrate/20150320164930_create_follow_table.rb
@@ -1,6 +1,6 @@
 class CreateFollowTable < ActiveRecord::Migration
   def up
-  	create_table :follow_connctions do |t|
+  	create_table :follow_connections do |t|
   		t.integer :user_id
   		t.integer :followed_user_id
   	end
